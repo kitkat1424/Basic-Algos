b15f0b291965106a22f700449ce81c8be468622c
kitkat1424 <126668725+kitkat1424@users.noreply.github.com>
2023-08-15 11:15:53 +0530

    Update insertionsort.cpp

--- Sorting Algorithms/insertionsort.cpp
+++ Sorting Algorithms/insertionsort.cpp
@@ -1,19 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void reverseInsertionSort(int* arr, int len);
+void InsertionSort(int* arr, int len);
+
 int main()
 {
-    int arr[10] = {10, 9, 8, 7, 6, 5 , 4, 3, 2, 1};
-    for(int i=1; i<10; i++){
-        int key = arr[i];
-        int j = i-1;
-        while(j >= 0 && arr[j] > key){
-            arr[j+1] = arr[j];
-            j--;
-        }
-        arr[j+1] = key;                                         
+    int t;
+    cin >> t;
+    int arr[t];
+    for(int i=0; i<t; i++){
+        cin >> arr[i];
     }
+    reverseInsertionSort(arr, t);
+    cout << "Decreasing Order \n";
+    for(int i:arr){
+        cout << i << " ";
+    }
+    InsertionSort(arr, t);
+    cout << '\n' << "Increasing Order \n";
     for(int i:arr){
         cout << i << " ";
     }
     return 0;
+}
+
+void reverseInsertionSort(int* arr, int len){
+    for(int i=1; i<len; i++){
+        int key = arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j] < key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
+
+void InsertionSort(int* arr, int len){
+    for(int i=1; i<len; i++){
+        int key = arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
 }
